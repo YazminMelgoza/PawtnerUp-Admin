bf99cfeb336aeb9c9ca063ffda8483e0aae888f5
jorgeein <77907542+jorgeein@users.noreply.github.com>
2024-05-20 18:24:05 -0600

    OKOJI

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
+import 'package:pawtnerup_admin/provider/auth_provider.dart';
 import 'package:flutter/material.dart';
 import 'package:pawtnerup_admin/config/config.dart';
 import 'package:pawtnerup_admin/config/router/app_router.dart';
 
-//Firebase Imports
+// Firebase Imports
 import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
 import 'firebase_options.dart';
+
+// Hive Imports
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  //DataBase Connection
+
+  // Firebase Initialization
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  // Hive Initialization
+  await Hive.initFlutter();
+  await Hive.openBox('userBox');
+
   runApp(const MyApp());
 }
 
@@ -20,11 +31,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      title: 'Pawtner Up',
-      routerConfig: appRouter,
-      theme: AppTheme().getTheme(),
-      debugShowCheckedModeBanner: false,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthenticationProvider())
+      ],
+      child: MaterialApp.router(
+        title: 'Pawtner Up',
+        routerConfig: appRouter,
+        theme: AppTheme().getTheme(),
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
